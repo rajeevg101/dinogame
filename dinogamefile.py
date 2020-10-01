e0aed4389f799581d7843e9abae06501fb73fd78
rajeevg101 <72181625+rajeevg101@users.noreply.github.com>
2020-10-01 09:49:59 +0530

    Create dinogamefile.py

---
+++ dinogamefile.py
@@ -0,0 +1,56 @@
+import pyautogui 
+# pip install pyautogui
+from PIL import Image, ImageGrab 
+# pip install pillow
+
+# from numpy import asarray
+import time
+
+def hit(key):
+    pyautogui.keyDown(key)
+    return
+
+def isCollide(data):
+    # rectanglr for cactus
+    for i in range(203, 250):
+        for j in range(770, 855):
+            if data[i, j] < 50:
+                hit("up")
+                return
+
+    # Draw the rectangle for birds
+    for i in range(185, 230):
+        for j in range(670, 770):
+            if data[i, j] < 50:
+                hit("down")
+                return
+    return
+
+if __name__ == "__main__":
+    print("Hey.. Dino game about to start in 4 seconds")
+    time.sleep(3)
+    #hit('up') 
+
+    while True:
+        image = ImageGrab.grab().convert('L')  
+        data = image.load()
+        isCollide(data)
+            
+        # print(asarray(image))
+        
+        # # Draw the rectangle for cactus
+        # for i in range(203, 250):
+        #     for j in range(770, 855):
+        #         data[i, j] = 120
+        
+        # # Draw the rectangle for birds
+        # for i in range(185, 230):
+        #     for j in range(670, 770):
+        #         data[i, j] = 0
+
+        # image.show()
+        # break
+        #chrome://dino/
+        
+        
+
